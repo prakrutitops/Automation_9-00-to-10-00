25a9dedaa834be8ce8b811c4d578460d4901b345
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-04-08 09:54:51 +0530

    Add files via upload

---
+++ Module1/8-4/ArrayListEx.java
@@ -0,0 +1,56 @@
+package com.a84;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListEx 
+{
+	
+	//add
+	//addAll
+	//remove
+	//removeAll
+	//retainAll
+	
+	
+	public static void main(String[] args) 
+	{
+		//dynamically
+		ArrayList<String>arrayList =new ArrayList<>();
+		
+		arrayList.add("A");
+		arrayList.add("B");
+		arrayList.add("C");
+		arrayList.add("D");
+		
+		
+		ArrayList<String>arrayList2 =new ArrayList<>();
+		
+		arrayList2.add("P");
+		arrayList2.add("Q");
+		arrayList2.add("R");
+		arrayList2.add("S");
+		arrayList2.add("B");
+		arrayList2.add("C");
+		arrayList2.add("D");
+		
+		//System.out.println(arrayList);
+		
+		/*
+		 * arrayList.addAll(arrayList2);
+		 * 
+		 * arrayList.remove(1); arrayList.removeAll(arrayList2);
+		 */
+		
+		arrayList.retainAll(arrayList2);
+		
+		Iterator i = arrayList.iterator();
+		
+		
+		while(i.hasNext()) //Loop
+		{
+			System.out.println(i.next());
+		}
+		
+	}
+}
---
+++ Module1/8-4/WrapperClassEx.java
@@ -0,0 +1,18 @@
+package com.a84;
+
+public class WrapperClassEx 
+{
+	public static void main(String[] args) 
+	{
+		
+		String a="1";
+		String b="2";
+		
+		
+		int c= Integer.parseInt(a)+Integer.parseInt(b);
+		System.out.println(c);
+		
+		//String.valueof();
+		
+	}
+}
