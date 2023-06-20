a59d6023f2603104e201241d90f48ed673ec840d
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-06-20 09:58:56 +0530

    Add files via upload

---
+++ Module5-Automation/20-6-maven/S001_browserDemo.java
@@ -0,0 +1,21 @@
+package P.Q;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class S001_browserDemo {
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		//driver.close();
+		//driver.quit();
+	}
+}
